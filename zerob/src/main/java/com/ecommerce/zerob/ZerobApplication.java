719133f5c2a668a1856b5f4f4a23368914e5f15c
SergiuZoitanu98 <szoitanu98@gmail.com>
2023-05-24 09:07:32 +0200

    backend

---
+++ zerob/src/main/java/com/ecommerce/zerob/ZerobApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.zerob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerobApplication.class, args);
+	}
+
+}
